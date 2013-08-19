f6c1e18f2ab1d4e50bcaac66b7a39f0e53b2eb58
alexrook <rook.alex@gmail.com>
2013-08-19 15:33:32 +0300

    added arquillian test infrastructure
    work with jboss-as-arquillian-container-managed
    with one dummy test;

---
+++ vio-se/src/test/java/vio/service/doc/ThemeFacadeTest.java
@@ -0,0 +1,49 @@
+package vio.service.doc;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.exporter.ZipExporter;
+import org.jboss.shrinkwrap.api.formatter.Formatters;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.runner.RunWith;
+import vio.model.doc.Theme;
+import vio.service.AbstractFacade;
+
+/**
+ * @author moroz
+ */
+@RunWith(Arquillian.class)
+public class ThemeFacadeTest {
+
+    public ThemeFacadeTest() {
+    }
+
+    @Deployment
+    public static Archive<?> createTestArchive() throws FileNotFoundException {
+
+        Archive<JavaArchive> result = ShrinkWrap.create(JavaArchive.class, "test.jar")
+                .addPackage(ThemeFacadeTest.class.getPackage())
+                .addPackage(AbstractFacade.class.getPackage())
+                .addPackage(Theme.class.getPackage())
+                .addAsResource("META-INF/persistence.xml", "META-INF/persistence.xml")
+                .addAsResource("test-ds.xml", "test-ds.xml")
+                .addAsResource("log4j.properties", "log4j.properties");
+
+        result.as(ZipExporter.class).exportTo(new File("target/test-ear.zip"), true);
+        //result.writeTo(new FileOutputStream("target/test-ear-arquillian.zip"), Formatters.VERBOSE);
+
+        return result;
+    }
+
+    @Test
+    public void dummy() {
+        assertTrue(true);
+    }
+}
