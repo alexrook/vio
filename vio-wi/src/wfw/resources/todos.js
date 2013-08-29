c5af05042cf0d73500a0caade94f9f07a3e85a78
alexrook <rook.alex@gmail.com>
2013-08-29 11:22:02 +0300

    add angular primer
    to run, cd vio-wi
    echo "postgresql.db.url=<db-connection-url>" >>local.properties
    mvn clean package -Pbuild-test-war jboss-as:run -Djboss-as.home=/path/to/jboss
    
    open http://localhost:8080/vio-wi-1.0-SNASHOT
    
    WARN! postgresql jdbc driver must be installed on jboss.

--- vio-wi/src/wfw/resources/todos.js
+++ vio-wi/src/wfw/resources/todos.js
@@ -1,8 +1,10 @@
-[
-    {text: 'learn angular', done: true},
-    {text: 'build an angular app', done: false},
-    {text: 'build an vio app', done: false},
-    {text: 'bay your moto', done: false},
-    {text: 'build an your big house', done: false},
-    {text: 'build your own life', done: false}
+{
+    "d":[
+    {"text": "learn angular", "done": "true"},
+    {"text": "build an angular app", "done": "false"},
+    {"text": "build an vio app", "done": "false"},
+    {"text": "bay your moto", "done": "false"},
+    {"text": "build an your big house", "done": "false"},
+    {"text": "build your own life", "done": "false"}
 ]
+}
