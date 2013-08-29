657edccd7bff1e88a52d33fb3385f176371339a5
alexrook <rook.alex@gmail.com>
2013-08-29 09:16:04 +0300

    add plain server json for test purposes

---
+++ vio-wi/src/wfw/resources/todos.js
@@ -0,0 +1,8 @@
+[
+    {text: 'learn angular', done: true},
+    {text: 'build an angular app', done: false},
+    {text: 'build an vio app', done: false},
+    {text: 'bay your moto', done: false},
+    {text: 'build an your big house', done: false},
+    {text: 'build your own life', done: false}
+]
