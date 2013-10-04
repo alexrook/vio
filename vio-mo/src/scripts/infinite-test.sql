5cbf59ccff7b06994ee02662ce50a22714535258
alexrook <rook.alex@gmail.com>
2013-10-04 10:42:45 +0300

    start work with infinite scroll table

---
+++ vio-mo/src/scripts/infinite-test.sql
@@ -0,0 +1,11 @@
+/* 'infinite scroll' test data */
+drop table infinite_test;
+
+create table infinite_test (id integer,value varchar(500));
+
+alter table infinite_test add constraint pk_ini primary key (id);
+
+\echo "Warn,the next command to add about 150 megabytes of test data"
+\echo "Do not forget to remove them through the 'delete from infinite_test'"
+
+insert into infinite_test select s.a,'biiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiig value_'||s.a::varchar from generate_series(0,999999) as s(a);
