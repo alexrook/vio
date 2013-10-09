c98958f5278f9731665a383287151efb6c940133
alexrook <rook.alex@gmail.com>
2013-10-09 15:43:41 +0300

    infinite scroll - array pushing with scroll event, sample from jsfiddle

--- vio-mo/src/scripts/infinite-test.sql
+++ vio-mo/src/scripts/infinite-test.sql
@@ -9,3 +9,9 @@ alter table infinite_test add constraint pk_ini primary key (id);
 \echo "Do not forget to remove them through the 'delete from infinite_test'"
 
 insert into infinite_test select s.a,'biiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiig value_'||s.a::varchar from generate_series(0,999999) as s(a);
+
+create or replace view v_test_infi
+as select max(id) from infinite_test
+union select min(id) from infinite_test
+order by 1;
+
