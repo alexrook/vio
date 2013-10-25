567c0f8db16718379f9dc44d7d4ec2ccf45c87ab
alexrook <rook.alex@gmail.com>
2013-10-25 14:05:47 +0300

    CORS&JSONP filter works
    для упрощенной js-разработки
    1. отредактировать при необходимости exposeHeadersList in web.xml  собрать и установить war
    mvn clean package -Pbuild-test-war
        jboss-as:deploy -Djboss-as.hostname=... -Djboss-as.username=admin -Djboss-as.password=...
    
    2. запустить в папке resources webapp
       легкий node-based сервер nserver
    
    3. иправить все url в *.js на адрес сервера jboss
    
    war приложение будет отвечать на cors-запросы дублируя значения
    в заголовках Access-Control-Allow-*
    в ответ на Origin и Access-Control-Request-*

--- vio-wi/src/main/java/vio/service/rest/filters/CORSandJSONPFilter.java
+++ vio-wi/src/main/java/vio/service/rest/filters/CORSandJSONPFilter.java
@@ -8,15 +8,36 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
+ * This filter wraps CORS and JSONP requests. Mainly for debugging purposes.
+ * Note: this opens a security hole on the site
+ *
+ * Suppose there is REST service /doc that
+ * returns an JSON in form: {"field":"value"}.
+ * If you activate this filter
+ * the request is in the form of /doc?callback = someFunc
+ * return
+ * someFunc({"field":"value"})
+ *
+ * @see http://ru.wikipedia.org/wiki/JSONP
+ * @see https://developer.mozilla.org/en-US/docs/HTTP/Access_control_CORS
  * @author moroz
  */
 public class CORSandJSONPFilter extends GenericFilter {
 
-    //this parameter must be in web.xml
+    //this parameters must be in web.xml
     public static final String JSONP_PARAMETER_NAME = "JSONPParameter";
-    //https://developer.mozilla.org/en-US/docs/HTTP/Access_control_CORS
-    public static final String REQUEST_ORIGIN_HEADER = "Origin";
-    public static final String RESPONSE_ALLOW_ORIGIN = "Access-Control-Allow-Origin";
+    public static final String EPOSE_HEADERS_PARAMETER_NAME = "exposeHeadersList";
+    //cors support
+    public static final String //request CORS headers
+            REQUEST_ORIGIN_HEADER = "Origin",
+            REQUEST_ALLOW_HEADERS = "Access-Control-Request-Headers",
+            //request CORS headers
+            RESPONSE_ALLOW_ORIGIN = "Access-Control-Allow-Origin",
+            RESPONSE_ALLOW_METHODS = "Access-Control-Allow-Methods",
+            RESPONSE_ALLOW_HEADERS = "Access-Control-Allow-Headers",
+            RESPONSE_ALLOW_AGE = "Access-Control-Max-Age",
+            RESPONSE_EXPOSE_HEADERS = "Access-Control-Expose-Headers",
+            RESPONSE_ALLOW_CREDENTIALS = "Access-Control-Allow-Credentials";
 
     public CORSandJSONPFilter() {
     }
@@ -30,11 +51,25 @@ public class CORSandJSONPFilter extends GenericFilter {
         String jsonPadding = getJsonPadding(request);
 
         HttpServletResponse httpResponse = (HttpServletResponse) response;
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
 
         if ((jsonPadding != null) && (jsonPadding.length() > 0)) {
             httpResponse.getOutputStream().println(jsonPadding + "(");
+        } else if (getOrigin(httpRequest) != null) {
+
+            // this security hole for debug&devel purposes 
+            setOriginHeaders(httpRequest, httpResponse);// in case Request Method:GET we need before processing
         }
 
+    }
+
+    private void setOriginHeaders(HttpServletRequest httpRequest, HttpServletResponse httpResponse) {
+        httpResponse.setHeader(RESPONSE_ALLOW_ORIGIN, httpRequest.getHeader(REQUEST_ORIGIN_HEADER));
+        httpResponse.setHeader(RESPONSE_ALLOW_METHODS, "*");
+        httpResponse.setHeader(RESPONSE_ALLOW_HEADERS, httpRequest.getHeader(REQUEST_ALLOW_HEADERS));
+        httpResponse.setHeader(RESPONSE_EXPOSE_HEADERS, getExposeHeadersList());
+        httpResponse.setHeader(RESPONSE_ALLOW_CREDENTIALS, "true");
+        httpResponse.setHeader(RESPONSE_ALLOW_AGE, "1728000");
     }
 
     @Override
@@ -42,7 +77,7 @@ public class CORSandJSONPFilter extends GenericFilter {
             throws IOException, ServletException {
 
         log("JSONPFilter:DoAfterProcessing");
-        
+
         HttpServletRequest httpRequest = (HttpServletRequest) request;
         HttpServletResponse httpResponse = (HttpServletResponse) response;
 
@@ -50,12 +85,17 @@ public class CORSandJSONPFilter extends GenericFilter {
 
         if ((jsonPadding != null) && (jsonPadding.length() > 0)) {
 
-            response.getOutputStream().println(")");
-        }
+            response.getOutputStream().println(")"); //close function bracket
 
-        if (getOrigin(httpRequest) != null) {
-            httpResponse.setHeader(RESPONSE_ALLOW_ORIGIN, "*");
+        } else if (getOrigin(httpRequest) != null) {
+            // this security hole for debug&devel purposes 
+            setOriginHeaders(httpRequest, httpResponse);// in case Request Method:GET we need after processing
         }
+    }
+
+    private String getExposeHeadersList() {
+        String ret = getFilterConfig().getInitParameter(EPOSE_HEADERS_PARAMETER_NAME);
+        return ret != null ? ret : "";
     }
 
     private String getJsonPadding(ServletRequest request) {
