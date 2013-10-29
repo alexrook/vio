2e6f275ec6d5572a00158a15d44e5837ed4807d1
alexrook <rook.alex@gmail.com>
2013-10-29 15:55:49 +0200

    work with tree

--- vio-wi/src/wfw/resources/js/ng-tree-menu.js
+++ vio-wi/src/wfw/resources/js/ng-tree-menu.js
@@ -4,7 +4,10 @@ var mod = angular.module('tree-menu', []);
 mod.directive('treeMenu', function() {
     return {
         /*
-         restrict: 'E',
+         *  When you create a directive, it is restricted to attribute only by default. 
+         * In order to create directives that are triggered by element name, 
+         * you need to use the restrict option.
+         restrict: 'A',
          scope: {
          customer: '=customer'
          },
